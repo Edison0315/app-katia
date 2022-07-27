74892d021e0539b8e70a3e6e922acac8ad95db54
Gerardo Bedoya <edison0315>
2022-07-27 06:25:26 -0500

    Subiendo a heroku 3

--- models/server.js
+++ models/server.js
@@ -9,7 +9,7 @@ class Server {
     constructor(){
 
         this.app  = express()
-        this.port = 8080
+        this.port = process.env.KATIA_PORT || 3000
 
         // Paths obj
         this.paths = {
